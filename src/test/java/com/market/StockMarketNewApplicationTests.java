854b6e7d628f274742b148a56ed27771d87a592c
Sujit <sujitstiwari11@gmail.com>
2022-05-04 12:42:57 +0530

    Test removed

--- src/test/java/com/market/StockMarketNewApplicationTests.java
+++ src/test/java/com/market/StockMarketNewApplicationTests.java
@@ -1,13 +1,9 @@
 package com.market;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StockMarketNewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
